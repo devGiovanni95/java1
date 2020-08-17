307ff5c7d6e8b07cc5b1562c8c66483c6a8db9b8
devGiovanni95 <giovanni.devjava@hotmail.com>
2020-08-17 20:45:36 -0300

    codigo de conversão de idade em anos para dias

--- operadoresAritimeticos.java
+++ operadoresAritimeticos.java
@@ -1,1 +1,17 @@
-# java
+import java.util.scanner;
+public class operadoresAritimeticos{
+    public static void main(String[] args) {
+        Scanner dados = new Scanner(System.in);
+
+        System.out.println("Digite sua idade em anos:");
+        int idade = dados.nextLine;
+        System.out.println("Digite os meses: ");
+        int meses = dados.nextLine;
+        System.out.println("Digite os dias: ");
+        int dias = dados.nextLine;
+
+        int idadeEmDias = idade * 365 + meses * 30 + dias;
+        System.out.println("Sua idade em dias são:");
+        System.out.println(idadeEmDias + " dias.");
+    }
+}
